e6ff4f8c48d29f9beae9f30df56a375afb9d8e92
Marios Kanellopoulos <mariosknl@outlook.com>
2026-02-23 12:06:49 +0200

    implement a Hoc for managing windows states

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
+import WindowControls from "#components/WindowControls.jsx";
 
 export {
     Welcome,
     Navbar,
     Dock,
+    WindowControls
 }
